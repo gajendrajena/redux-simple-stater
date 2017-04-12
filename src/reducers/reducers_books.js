d70d56906cb8c982f6d982bd694968ae32f9bcef
Gajendra Jena <gjena@ctepl.com>
2017-04-12 15:24:20 +0530

    first commit

---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,11 @@
+export default function(){
+  return [
+    {title: 'Javascript: the good parts'},
+    {title: 'Javascript: the good parts1'},
+    {title: 'Javascript: the good parts2'},
+    {title: 'Javascript: the good parts3'},
+    {title: 'Javascript: the good parts4'},
+    {title: 'Javascript: the good parts5'},
+    {title: 'Javascript: the good parts6'}
+  ]
+}
